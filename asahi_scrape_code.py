92a5301e7bbb19cbfcf40aeeba175b6d58c41fe5
Justin Reeves <103855000+Justin-f-Reeves@users.noreply.github.com>
2022-11-20 15:14:52 -0500

    Update asahi_scrape_code.py

--- asahi_scrape_code.py
+++ asahi_scrape_code.py
@@ -1,24 +1,3 @@
-# A script to scrape legislative candidate survey data from this site:
-# https://www.asahi.com/senkyo/shuinsen/2021/asahitodai/ 
-# There are over 25,000 observations to record.
-
-# I use a webdriver (Selenium) because the site uses session cookies to 
-# autheticate users on every single page (over 800) that's requested.
-# You'll need to first create a free account on the site, using Chrome as
-# your browser, and then revisit the main survey page before running 
-# the script.
-
-# The script navigates to each candidate's page direcly via a URL, rather
-# than making Selenium interact with the site's nav system, and scrapes 
-# these pages in a loop. The URLs were scraped in a seperate script and
-# saved to a CSV file (cands_urls.csv), which is imported here. This is 
-# much faster and more reliable approach as it avoids the intermediary 
-# steps of switching window tab focus and clicking on nav buttons for
-# every new set of candidates.  
-
-# I use artificial time delays to be more friendly to Asahi's server, 
-# with randomly generated time intervals to make the requests seem human. 
-
 import time
 import random
 from selenium import webdriver
